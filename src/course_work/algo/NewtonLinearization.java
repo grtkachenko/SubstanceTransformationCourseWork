bde1b1938fe0e5b462a147e902c61ab3b9d16246
Grigory Tkachenko <grtkachenko@gmail.com>
2015-01-22 23:43:34 +0300

    Updated params in NewtoneLin

--- src/course_work/algo/NewtonLinearization.java
+++ src/course_work/algo/NewtonLinearization.java
@@ -46,9 +46,9 @@ public class NewtonLinearization implements ComputationMethod<double[]> {
             final double[] c = new double[n];
             final double[] d = new double[n];
             for (int i = 0; i < n; i++) {
-                a[i] = c[i] = 0/*here*/;
-                d[i] = 0/*here*/;
-                b[i] = 0/*here*/;
+                a[i] = c[i] = s.D / s.dz / s.dz;
+                b[i] = - 2 * s.D / s.dz / s.dz - 1 / s.dt - Utils.wWithoutOneX(X[i], T[i], s) * s.alpha;
+                d[i] = X[i] * (- 1 / s.dt - Utils.wWithoutOneX(X[i], T[i], s) * (1 - s.alpha));
             }
             a[n - 1] = c[0] = 0;
             b[0] = b[n - 1] = 1;
@@ -63,9 +63,9 @@ public class NewtonLinearization implements ComputationMethod<double[]> {
             final double[] c = new double[n];
             final double[] d = new double[n];
             for (int i = 0; i < n; i++) {
-                a[i] = c[i] = 0/*here*/;
-                d[i] = 0/*here*/;
-                b[i] = 0/*here*/;
+                a[i] = c[i] = s.kappa / s.dz / s.dz;
+                b[i] = - 2 * s.kappa / s.dz / s.dz - 1 / s.dt + s.Q / s.C * Utils.w(newX[i], T[i], s) * s.E / s.R / (T[i] * T[i]);
+                d[i] = - T[i] / s.dt - s.Q / s.C * Utils.w(newX[i], T[i], s) * (1 - s.E / s.R / T[i]);
             }
             a[n - 1] = c[0] = 0;
             b[0] = b[n - 1] = 1;
