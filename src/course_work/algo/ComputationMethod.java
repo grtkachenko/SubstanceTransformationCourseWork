591b5d98ad7249e4392171c86e06829405164739
Grigory Tkachenko <grtkachenko@gmail.com>
2015-01-22 19:39:59 +0300

    fix

--- src/course_work/algo/ComputationMethod.java
+++ src/course_work/algo/ComputationMethod.java
@@ -4,6 +4,9 @@ package course_work.algo;
  * Created by antonkov on 1/18/2015.
  */
 public interface ComputationMethod<T> {
+    // equations:    dX/dt -     D d2X/dz2 =      w(X, T)
+    //               dT/dt - kappa d2T/dz2 = -Q/C w(X, T)
+    //               w(X, T) = -K X^alpha exp(-E/RT)
     public void init(Settings settings);
 
     public Settings getSettings();
